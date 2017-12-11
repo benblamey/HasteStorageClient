515f1c17065cd88caf36b6cd0a3608966eb131e8
Ben Blamey <blamey.ben@gmail.com>
2017-12-11 21:15:08 +0100

    remove version from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 from distutils.core import setup
 
 setup(name='haste_storage_client',
-      version='0.2',
       py_modules=['haste_storage_client'],
       install_requires=[
           'pymongo',
