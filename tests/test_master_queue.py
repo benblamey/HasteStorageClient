acb40fe3bcdd64e194d2e03ccd09aee57e4ae152
Ben Blamey <blamey.ben@gmail.com>
2019-11-25 13:30:13 +0100

    fix tests

--- tests/test_master_queue.py
+++ tests/test_master_queue.py
@@ -1,14 +1,35 @@
+import sys
+
 import numpy as np
 from haste_storage_client.haste_priority_queue_client import MODE_GOLDEN, HastePriorityQueueClient, STATE_NONE, STATE_IN_QUEUE_NOT_PRE_PROCESSED, STATE_PRE_PROCESSING, STATE_POPPING, STATE_POPPED, \
     STATE_IN_QUEUE_PRE_PROCESSED
 
 
 def test_master_queue_golden():
-    q = HastePriorityQueueClient(5, MODE_GOLDEN, np.array([1, 2, 3, 4, 5], dtype=np.float))
+    if sys.version_info[0] == 2 or sys.version_info[1] < 6:
+        # MasterQueue only supports Python 3.6 or above.
+        # TODO: investigate fixing this.
+        return
+
+    class MockInterestingnessModel:
+        def interestingness(self,
+                            stream_id=None,
+                            timestamp=None,
+                            location=None,
+                            substream_id=None,
+                            metadata=None,
+                            mongo_collection=None):
+            pass
+
+    q = HastePriorityQueueClient(5, MODE_GOLDEN, np.array([1, 2, 3, 4, 5], dtype=np.float), interestingess_model=MockInterestingnessModel())
     assert ((q.states == STATE_NONE).all())
 
     for i in range(5):
-        q.push({})
+        q.save(1,
+               1,
+               None,
+               None,
+               {})
 
     assert ((q.states == STATE_IN_QUEUE_NOT_PRE_PROCESSED).all())
 
